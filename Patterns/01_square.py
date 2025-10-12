8555cd45fac7d57a11737163f1e8df80ab0bda1d
Onkar Ghadage <147942562+OnkarGhadage@users.noreply.github.com>
2025-10-12 21:19:16 +0530

    Create 01_square.py
    Square pattern

---
+++ Patterns/01_square.py
@@ -0,0 +1,10 @@
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+n = int(input("Enter size: "))   #Taking i/p of size to print particular size of square
+
+for i in range(n):   #for loo[
+    print("* "*n)   #Print '* ' i times times
